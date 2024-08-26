72776cf92e8cc62d58994c9d427c80a1fb4b9caf
fanmo <110224569+fischerluMS@users.noreply.github.com>
2024-08-26 18:33:27 +0800

    Add files via upload

---
+++ test_8.26.cpp
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h> 
+int main()
+{
+	int a = 0;
+	int b = 0;
+	int sum = 0;
+	scanf("%d %d" , &a,&b);
+	sum = a + b;
+	printf("sum = %d\n", sum );
+	return 0;
+}
